74dcd8986df007d71fb8f62550d63b443cd971ac
Taco-C <perian.forod@gmail.com>
2020-03-01 00:17:14 +0100

    No need for Table{}s

--- database.go
+++ database.go
@@ -4,35 +4,14 @@ import (
 	"bufio"
 	"errors"
 	"fmt"
-	"log"
 	"os"
-	"path/filepath"
 	"strings"
 )
 
 // Database holds information concerning a database.
 // A database is just a folder in which the tables (csv files) live.
 type Database struct {
-	Path  string
-	Table map[string]*Table
-}
-
-// Create creates a new database on the filesystem,
-// and returns a Database object.
-func Create(path string) (*Database, error) {
-	exists, err := fileExists(path)
-	if err != nil {
-		return &Database{}, err
-	}
-
-	if exists {
-		e := fmt.Sprintf("Could not create new database: %s: file already exists.", path)
-		return &Database{}, errors.New(e)
-	}
-
-	fmt.Println("Creates dir")
-	os.MkdirAll(path, 0755)
-	return &Database{path, make(map[string]*Table)}, nil
+	Path string
 }
 
 // Open opens an existing database on the filesystem,
@@ -48,28 +27,7 @@ func Open(path string) (*Database, error) {
 		return &Database{}, errors.New(e)
 	}
 
-	// Loop through csv-files and add them as tables.
-	// tableFiles := make([]os.FileInfo, 0)
-
-	db := &Database{}
-	tables := make(map[string]*Table)
-	err = filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
-		if info.IsDir() {
-			return nil
-		}
-		tables[info.Name()] = NewTable(info.Name(), path, db)
-		return nil
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return &Database{path, tables}, nil
-}
-
-// CreateTable creates a csv file in the database directory.
-func (db *Database) CreateTable(name string) {
-	db.Table[name] = &Table{}
+	return &Database{path}, nil
 }
 
 // From gets all the rows from the file.
